2e34a21fa82cea3f09decf5ff4b2b0f9fa65c38c
mjmiossec <m.j.miossec@gmail.com>
2017-07-11 16:21:13 -0400

    Added a constructor for the BIOM format. Made slight changes to constructor for the phyloseq format. added biomformat library to list of imports.

--- R/GenericOmicSet-class.R
+++ R/GenericOmicSet-class.R
@@ -67,11 +67,20 @@ setMethod("dim", "GenericOmicSet",
 #         metadata=as.list(metadata))
 # }
 
-read_phyloseq <- function(phylobject){
+readinphyloseq <- function(phylobject){
   if (isClass(phylobject,"phyloseq")){
     sampleMetadata <- as(as(phyloseq::sample_data(phylobject),"data.frame"),"DataFrame")
     featureMetadata <- as(as(phyloseq::tax_table(phylobject),"matrix"),"DataFrame")
     assay=SummarizedExperiment::Assays(S4Vectors::SimpleList(as(phyloseq::otu_table(phylobject),"matrix")))
-    genericomicset <- new("GenericOmicSet",name="phyloseq",sampleMetadata=sampleMetadata,featureMetadata=featureMetadata,assays=assay)
+    genericomicset <- new("GenericOmicSet",name="phyloIn",sampleMetadata=sampleMetadata,featureMetadata=featureMetadata,assays=assay)
   }
+}
+
+readinbiom <- function(biomobject){
+  ##if statement will go here...
+    sampleMetadata <- as(biomformat::sample_metadata(biomobject),"DataFrame")
+    featureMetadata <- as(biomformat::observation_metadata(biomobject),"DataFrame")
+    assay=SummarizedExperiment::Assays(S4Vectors::SimpleList(as(biomformat::biom_data(biomobject),"matrix")))
+    genericomicset <- new("GenericOmicSet",name="biomIn",sampleMetadata=sampleMetadata,featureMetadata=featureMetadata,assays=assay)
+  ##and end here...
 }

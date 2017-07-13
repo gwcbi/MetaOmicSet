103a4e9e488d859a9aef460612e414b9fe7ef823
Matthew Bendall <bendall@gwu.edu>
2017-07-13 19:29:12 -0400

    uid2lineage can accept ranks, included tests.

--- R/uid2lineage.R
+++ R/uid2lineage.R
@@ -1,7 +1,33 @@
-library(taxize)
-library(plyr)
 
-uid2lineage<-function(x){
+.RANKS.PROK <- c('superkingdom', 'phylum', 'class', 'order', 'family', 'genus',
+                'species')
+
+.RANKS.EUK <- c('superkingdom', 'kingdom', 'phylum', 'class', 'order', 'family',
+               'genus', 'species')
+
+.RANKS.VIRUS <- c('superkingdom', 'order', 'family', 'genus', 'species')
+
+
+#' Download lineage for NCBI taxonomy ID
+#' 
+#' Uses taxize to download the lineage for given taxon IDs
+#' 
+#' @param x Vector of taxa names (character) or IDs (character or numeric) to 
+#'   query. Passed to \code{\link[taxize]{classification}}.
+#' @param ranks Vector of rank labels for lineage.
+#' 
+#' @return A data.frame with the lineage for each supplied taxonomy ID.
+#' @export
+#'
+#' @examples
+#' taxids <-c(39313, 2, 48381, 95953, 4923, 11111, 111111111)
+#' lineage.df <- uid2lineage(taxids)
+#' 
+uid2lineage <- function(x, 
+                        ranks=c('superkingdom', 'phylum', 'class', 'order',
+                                 'family', 'genus', 'species')
+                        )
+{
   
   #define the main error in case of the fetch fail
   tmp<-c("Error in FUN(X[[i]], ...) : Gateway Timeout (HTTP 504)")
@@ -11,52 +37,23 @@ uid2lineage<-function(x){
   
   #adding one slice of security
   while (grepl("Gateway Timeout",tmp) && retry>0) {
-    tryCatch({tmp<-classification(x, db = 'ncbi')}, error = function(e){
+    tryCatch({tmp<-taxize::classification(x, db = 'ncbi')}, error = function(e){
       print(e);print("retrying");tmp<-"Gateway Timeout"})
     retry=retry-1
   }
   
-  #get the df that contain the lineage
-  tmpdf<-as.data.frame(tmp[[1]])
-  if(nrow(tmpdf)>1){
-    superk<-ifelse(identical("character(0)",superk<-as.character(tmpdf[which(
-      tmpdf[2]=="superkingdom"),][1])),"NA",superk)
-    phylum<-ifelse(identical("character(0)",phylum<-as.character(tmpdf[which(
-      tmpdf[2]=="phylum"),][1])),"NA",phylum)
-    class<-ifelse(identical("character(0)",class<-as.character(tmpdf[which(
-      tmpdf[2]=="class"),][1])),"NA",class)
-    order<-ifelse(identical("character(0)",order<-as.character(tmpdf[which(
-      tmpdf[2]=="order"),][1])),"NA",order)
-    family<-ifelse(identical("character(0)",family<-as.character(tmpdf[which(
-      tmpdf[2]=="family"),][1])),"NA",family)
-    genus<-ifelse(identical("character(0)",genus<-as.character(tmpdf[which(
-      tmpdf[2]=="genus"),][1])),"NA",genus)
-    species<-ifelse(identical("character(0)",species<-as.character(tmpdf[which(
-      tmpdf[2]=="species"),][1])),"NA",species)
-    name<-as.character(tmpdf[nrow(tmpdf),][1])
-    out<-data.frame(ID=x,Superkingdom=superk,Phylum=phylum,Class=class,
-                    Order=order,Family=family,Genus=genus,Species=species,
-                    `Last Rank`=name)
-  }else{
-    out<-data.frame(ID=x,Superkingdom="NA",Phylum="NA",Class="NA",
-                                    Order="NA",Family="NA",Genus="NA",Species="NA",
-                                    Last.Rank="NA") 
-  }
-  return(out)
-  
+  missing.row <- c(sapply(ranks, function(.z) NA), Last.Rank=NA)
+  lapply(names(tmp), function(tid) {
+     if(is.null(nrow(tmp[[tid]]))) return(c(ID=tid, missing.row))
+      # Process data frame
+     ret <- tmp[[tid]]$name
+     names(ret) <- tmp[[tid]]$rank
+     ret <- ret[ranks]
+     names(ret) <- ranks
+     # Get the name of query
+     qname <- tmp[[tid]][tmp[[tid]]$id == tid, ]$name
+     c(ID=tid, ret, Last.Rank=qname)
+  }) %>%
+      do.call(rbind, .) %>%
+      data.frame(stringsAsFactors=F)
 }
-dflineage<-function(x){
-  out<-ldply(lapply(x,uid2lineage),data.frame)
-  out[out=="NA"]<-NA
-  return(out)
-}
-
-#############
-###EXAMPLE###
-#############
-
-thelist<-c(39313,2,48381,95953,4923,11111,111111111)
-lineages<-dflineage(thelist)
-lineages
-
-

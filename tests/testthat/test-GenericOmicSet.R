46c058ec4302b807745905511997a80a40220970
Mrigank Tiwari <tiwarimrigank2@gmail.com>
2017-07-17 12:21:27 -0400

    adding the test script

--- tests/testthat/test-GenericOmicSet.R
+++ tests/testthat/test-GenericOmicSet.R
@@ -3,5 +3,10 @@ context("Check GenericOmicSet")
 # x <- new("GenericOmicSet", name='myset')
 
 test_that("read_pathoscope", {
-  example_data_dir <- system.file(package = "MetaOmicSet")
+  example_dir <- system.file("extdata/asthma", package = "MetaOmicSet")
+  list(ex_Metagenome, ex_colData) <- read_pathoscope(dir_file = example_dir,
+                                               smeta_file = "Data-PRJNA255523.csv",
+                                               pattern1 = ".filtered.fastq-sam-report.tsv",
+                                               splitPoint = "[.]")
+  
 })

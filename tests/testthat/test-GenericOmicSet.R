b58e698031eef4f22ac1603002580df200b7aa65
Mrigank Tiwari <tiwarimrigank2@gmail.com>
2017-07-17 14:58:06 -0400

    updated the test for run_pathoscope

--- tests/testthat/test-GenericOmicSet.R
+++ tests/testthat/test-GenericOmicSet.R
@@ -4,9 +4,10 @@ context("Check GenericOmicSet")
 
 test_that("read_pathoscope", {
   example_dir <- system.file("extdata/asthma", package = "MetaOmicSet")
-  list(ex_Metagenome, ex_colData) <- read_pathoscope(dir_file = example_dir,
+  a <- read_pathoscope(dir_file = example_dir,
                                                smeta_file = "Data-PRJNA255523.csv",
                                                pattern1 = ".filtered.fastq-sam-report.tsv",
                                                splitPoint = "[.]")
-  
+  expect_equal(dim(a[[1]]), c(1508, 14))
+  expect_equal(dim(a[[2]]), c(14, 6))
 })

f56bb4d8f0ac34de8c798fecec9be56be9cd256f
Mrigank Tiwari <tiwarimrigank2@gmail.com>
2017-08-01 12:57:44 -0400

    lineage component of the class to the test file

--- tests/testthat/test-GenericOmicSet.R
+++ tests/testthat/test-GenericOmicSet.R
@@ -10,4 +10,5 @@ test_that("read_pathoscope", {
                                                splitPoint = "[.]")
   expect_equal(dim(a[[1]]), c(1508, 14))
   expect_equal(dim(a[[2]]), c(14, 6))
+  expect_equal(dim(a[[3]]), c(1508, 9))
 })

bdf26995c9630bc3cf9c86a882700bc4940609c4
mjmiossec <m.j.miossec@gmail.com>
2017-11-10 12:43:25 -0300

    Made small but consequential changes that ensure GenericOmicTest can be run:
    In GenericOmicTest class changed pattern1 to include the '.gz' extension
    
    In IO-methods class, added two references to 'dir_file' which was not previously being called.

--- tests/testthat/test-GenericOmicSet.R
+++ tests/testthat/test-GenericOmicSet.R
@@ -6,7 +6,7 @@ test_that("read_pathoscope", {
   example_dir <- system.file("extdata/asthma", package = "MetaOmicSet")
   a <- read_pathoscope(dir_file = example_dir,
                                                smeta_file = "Data-PRJNA255523.csv",
-                                               pattern1 = ".filtered.fastq-sam-report.tsv",
+                                               pattern1 = ".filtered.fastq-sam-report.tsv.gz",
                                                splitPoint = "[.]")
   expect_equal(dim(a[[1]]), c(1508, 14))
   expect_equal(dim(a[[2]]), c(14, 6))
